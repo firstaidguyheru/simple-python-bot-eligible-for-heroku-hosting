226a5a4044a1ae4da8de925b0a95db590bc9192e
Heru Jolly <jheru711@gmail.com>
2020-07-22 15:25:23 -0400

    ok

--- bot.py
+++ bot.py
@@ -1,18 +1,37 @@
 import discord
 from discord.ext import commands
-
+from asyncio import sleep
 client = commands.Bot(command_prefix='-', help_command=None)
 
+
+async def status():
+    while True:
+        await client.wait_until_ready()
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='1'))
+        await sleep(5)
+        await client.change_presence(activity=discord.Game(name='2'))
+        await sleep(5)
+        await client.change_presence(activity=discord.Game(name='3'))
+        await sleep(5)
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="I Am A Python Bot!"))
-    print('I have Awoken!')
+    print(f'{client.user} has Awoken!')
+client.loop.create_task(status())
+
 
 @client.command(name='hi', aliases=['Hi'])
 async def hi_cmd(ctx):
     await ctx.channel.send('hi bot user.')
 
-token = 'NzMwNjY0OTQzMTQ5MzgzNzAx.XxHnBQ.NSbmzNg44H3mWPbzXcfej5d8oUk'
+@client.event
+async def on_member_join(member):
+    mbed = discord.Embed(
+        colour = (discord.Colour.magenta()),
+        title = 'Welcome Message',
+        description = f'Welcome {member.mention}, enjoy your stay!'
+    )
+    await member.send(embed=mbed)
+token = 'NzMwNjY0OTQzMTQ5MzgzNzAx.XxUUSw.mtBvKry3fIlxHInFG4AcMG58fFA'
 
 {
     client.run(token)
