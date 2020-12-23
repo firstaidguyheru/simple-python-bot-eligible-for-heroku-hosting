fd21b2bc754c3504f6850bf4de1b164de54ec398
clvrk <52180876+clvrk@users.noreply.github.com>
2020-12-22 19:36:50 -0800

    Implementing proper security.

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
+import os
+from dotenv import load_env
 import discord
 from discord.ext import commands
 from asyncio import sleep
+
 client = commands.Bot(command_prefix='-', help_command=None)
 
+load_env()
 
 async def status():
     while True:
         await client.wait_until_ready()
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='1'))
-        await sleep(5)
+        await sleep(60*10)
         await client.change_presence(activity=discord.Game(name='2'))
-        await sleep(5)
+        await sleep(60*10)
         await client.change_presence(activity=discord.Game(name='3'))
-        await sleep(5) ## take off if you want.
+        await sleep(60*10) ## take off if you want.
         
 @client.event
 async def on_ready():
@@ -22,7 +26,7 @@ async def on_ready():
 
 @client.command(name='hi', aliases=['Hi'])
 async def hi_cmd(ctx):
-    await ctx.channel.send('hi bot user.')
+    await ctx.send('hi bot user.')
 
 @client.event
 async def on_member_join(member):
@@ -33,8 +37,6 @@ async def on_member_join(member):
     )
     await member.send(embed=mbed)
     
-token = ''
 
-{
-    client.run(token)
-}
+client.run(os.getenv('TOKEN'))
+
